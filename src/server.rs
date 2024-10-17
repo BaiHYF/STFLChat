3cc2e9c2ce9434ebb47fc8c1dff1497949dd3afc
BaiHYF <baiheyufei@gmail.com>
2024-10-17 12:26:31 +0800

    Feature: add a channel for chat

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,9 @@
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{net::{TcpListener, TcpStream},  sync::broadcast::{self, Sender}};
 use bytes::Bytes;
 use std::{collections::HashSet, sync::{Arc, Mutex}};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
-use futures::{SinkExt, StreamExt};
+use futures::{channel::oneshot::channel, SinkExt, StreamExt};
 
 const HELP_MSG: &str = "There is no help message yet";
 const WELCOM_MSG: &str = "Please enter your username: ";
@@ -17,25 +17,30 @@ pub async fn main() {
     let listener = TcpListener::bind("127.0.0.1:7474").await.unwrap();
 
     let users = UserNames::new(Mutex::new(HashSet::new()));
+    let (channel_sender, _) = broadcast::channel::<String>(32);
 
     loop {
         let (socket, addr) = listener.accept().await.unwrap();
         let users = Arc::clone(&users);
-
+        let channel_sender = channel_sender.clone();
         // 为每一条连接都生成一个新的任务，
         // `socket` 的所有权将被移动到新的任务中，并在那里进行处理
         println!("\x1b[34mSERVER:\x1b[0m accepted connection from: \x1b[32m{}\x1b[0m", addr);
         tokio::spawn(async move {
-            let _ = process_user(socket, users).await;
+            let _ = process_user(socket, users, channel_sender).await;
         });
     }
 }
 
-async fn process_user(mut socket: TcpStream, users : UserNames) -> anyhow::Result<()> {
+async fn process_user(
+    mut socket: TcpStream, 
+    users : UserNames,
+    channel_sender : Sender<String>,
+) -> anyhow::Result<()> {
     let (reader, writer) = socket.split();
     let mut stream = FramedRead::new(reader, LinesCodec::new());
     let mut sink = FramedWrite::new(writer, LinesCodec::new());
-    
+    let mut channel_recevier = channel_sender.subscribe();
     sink.send(WELCOM_MSG).await?;
     let mut name = String::new();
     while let Some(Ok(line)) = stream.next().await {
@@ -47,24 +52,37 @@ async fn process_user(mut socket: TcpStream, users : UserNames) -> anyhow::Resul
         }
     }
     users.lock().unwrap().insert(name.clone());
-    while let Some(Ok(line)) = stream.next().await {
-        if line.starts_with("/help") {
-            sink.send(HELP_MSG).await?;
-        } else if line.starts_with("/exit") {
-            break;
-        } else if line.starts_with("/users") {
-            let users = users.lock().unwrap().clone();
-            let mut msg = String::from("\x1b[34mOnline users:\x1b[0m \n");
-            for user in users.iter() {
-                msg += "   ";
-                msg += user;
-                msg += "\n ";
+
+    loop {
+        tokio::select! {
+            send_msg = stream.next() => {
+                let send_msg = send_msg.unwrap()?;
+                if send_msg.starts_with("/help") {
+                    sink.send(HELP_MSG).await?;
+                } else if send_msg.starts_with("/exit") {
+                    break;
+                } else if send_msg.starts_with("/users") {
+                    let users = users.lock().unwrap().clone();
+                    let mut msg = String::from("\x1b[34mOnline users:\x1b[0m \n");
+                    for user in users.iter() {
+                        msg += "   ";
+                        msg += user;
+                        msg += "\n";
+                    }
+                    sink.send(msg).await?;
+                } 
+                else {
+                    let msgback = format!("{}|{}", name, send_msg);
+                    let _ = channel_sender.send(msgback);
+                }
+            },
+            recv_msg = channel_recevier.recv() => {
+                // parse the message and find its reciver
+                // if the reciver is the current user, send it to the client
+
+                let recv_msg = recv_msg?;
+                sink.send(recv_msg).await?;
             }
-            sink.send(msg).await?;
-        } 
-        else {
-            let msgback = format!("From {}: {}", name, line);
-            sink.send(msgback).await?;
         }
     }
     anyhow::Ok(())
